1d831bd930169dd937e79d7326e22bb064b46776
Bishal Shrestha <iambstha@gmail.com>
2024-04-01 10:52:36 +0545

    commented

--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -4,12 +4,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+ 	Question 
+ 	Sort Data and Lists in an Array
+	Write Java code to sort each string and its list in an array. 
+	First, sort each string in the array, and then sort the lists associated with each string.
+
+	Input: 
+	["xL01(F]J","2pn5Mm","-5)8gF{","KWq0P]*%Q","n@,:\u003eAm@","\u003cRN_qCa7","8Qx\u0026RAON","gT~s!1s?4i{K","w\"r^d_#l$Mmp"]
+    
+	Output:
+    ["(01FJL]x","25Mmnp",")-58Fg{","%*0KPQW]q",",:\u003e@@Amn","7\u003cCNR_aq","\u00268ANOQRx","!14?KTgiss{~","\"#$M^_dlmprw"]
+*/
+
+/*
+	The Unicode interpretation by java compiler seems to not detect the Unicode character code that is
+	written inside the string literal. So this solution might not be the best way to solve this question.
+	But for the sake of this task, I have provided a solution which might not be the best solution.
+	If there is any better way to do this, I would be more that happy to learn.
+*/
+
 public class Main {
-	
-	// The Unicode interpretation by java compiler seems to not detect the unicode character code that is
-	// written inside the string literal. So this solution might not be the best way to solve this question.
-	// But for the sake of this task, I have provided a solution but this might not be the best solution.
-	// If there is any better way to do this, I would be more that happy to learn.
 
 	public static void main(String[] args) {
 		String[] qArray = { "xL01(F]J", "2pn5Mm", "-5)8gF{", "KWq0P]*%Q", "n@,:\u003eAm@", "\u003cRN_qCa7",
